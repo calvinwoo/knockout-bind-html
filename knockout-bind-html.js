24af353a9acc95352691942bb40092d0e6a5b600
Calvin Woo <calvin.woo@citrix.com>
2015-03-21 16:14:40 -0700

    Add a code comment. Reorganized code for clarity.

--- knockout-bind-html.js
+++ knockout-bind-html.js
@@ -1,19 +1,20 @@
 (function (root, factory) {
   if(typeof define === 'function' && define.amd) {
+    // Support AMD modules
     define(['knockout'], factory);
   } else {
     factory(root.ko);
   }
 }(this, function(ko) {
+  ko.bindingHandlers.bindHtml.preprocess = function(value, name, addBinding) {
+    addBinding('html', value);
+    return value;
+  };
+
   ko.bindingHandlers.bindHtml = {
     update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
       ko.unwrap(valueAccessor());
       ko.applyBindingsToDescendants(bindingContext.$data, element);
     }
-  };
-
-  ko.bindingHandlers.bindHtml.preprocess = function(value, name, addBinding) {
-      addBinding('html', value);
-      return value;
   };
 }));
